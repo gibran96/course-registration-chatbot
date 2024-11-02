e566d68dd044431edb0bb6ef3efccd24b7cf3202
Gibran <myageri.g@northeastern.edu>
2024-11-02 01:19:27 -0400

    reverted import

--- data_pipeline/dags/scripts/fetch_banner_data.py
+++ data_pipeline/dags/scripts/fetch_banner_data.py
@@ -10,7 +10,7 @@ import numpy as np
 from airflow.models import Variable
 import ast
 
-from data_pipeline.dags.scripts.extract_trace_data import clean_response
+from scripts.extract_trace_data import clean_response
 
 semester_map = {
     "10": "Fall",
