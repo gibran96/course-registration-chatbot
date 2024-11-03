e3aed05b835cca958d8ff8fd3321127b71bae24a
Gibran <myageri.g@northeastern.edu>
2024-11-03 15:36:58 -0500

    removed json dumps

--- data_pipeline/dags/scripts/fetch_banner_data.py
+++ data_pipeline/dags/scripts/fetch_banner_data.py
@@ -131,7 +131,7 @@ def get_courses_list(cookie_output):
     
     logging.info(f"Number of courses fetched: {len(course_data)}")    
 
-    return json.dumps(course_data)
+    return course_data
 
 # Function to fetch the faculty info from the Banner API
 def get_faculty_info(cookie_output, course_list):
@@ -166,12 +166,7 @@ def get_faculty_info(cookie_output, course_list):
         data = response.json()["fmt"][0]
         
         if response.status_code == 200:
-            # if faculty name is not present, remove the course from the list
-            if not data["faculty"]:
-                logging.warning(f"No faculty found for course: {course_ref_num}")
-                del course_list[course]
-                continue
-            course_list[course]["faculty_name"] = data["faculty"]["displayName"]
+            course_list[course]["faculty_name"] = data["faculty"]["displayName"] if data["faculty"] else ""
             course_list[course]["begin_time"] = data["meetingTime"]["beginTime"] if data["meetingTime"] else ""
             course_list[course]["end_time"] = data["meetingTime"]["endTime"] if data["meetingTime"] else ""
             course_list[course]["days"] = get_days(data["meetingTime"]) if data["meetingTime"] else ""
