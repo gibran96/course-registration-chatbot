2e8098be8c7f237d1898d83bb1970f23acabd4ec
Gibran <myageri.g@northeastern.edu>
2024-11-03 15:26:43 -0500

    corrected name

--- data_pipeline/dags/scripts/fetch_banner_data.py
+++ data_pipeline/dags/scripts/fetch_banner_data.py
@@ -160,7 +160,7 @@ def get_faculty_info(cookie_output, course_list):
         
         try:  
             response = requests.post(url, headers=headers, params=params)
-        except requests.exceptions.RequestException as e:
+        except Exception as e:
             logging.error(f"Failed to fetch faculty info for crn: {course_ref_num}. Error: {e}")
         
         data = response.json()["fmt"][0]
@@ -171,7 +171,7 @@ def get_faculty_info(cookie_output, course_list):
                 logging.warning(f"No faculty found for course: {course_ref_num}")
                 del course_list[course]
                 continue
-            course_list[course]["faculty_name"] = data["faculty"]["name"]
+            course_list[course]["faculty_name"] = data["faculty"]["displayName"]
             course_list[course]["begin_time"] = data["meetingTime"]["beginTime"] if data["meetingTime"] else ""
             course_list[course]["end_time"] = data["meetingTime"]["endTime"] if data["meetingTime"] else ""
             course_list[course]["days"] = get_days(data["meetingTime"]) if data["meetingTime"] else ""
