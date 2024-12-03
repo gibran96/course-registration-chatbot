3ad58f76d25484ce831ad2eab5753adc4e20d2e8
Gibran <myageri.g@northeastern.edu>
2024-12-02 20:01:09 -0500

    added code - for fetching context from bq embeddings - adding data to user data table

--- model_deployment/app/services/llm_service.py
+++ model_deployment/app/services/llm_service.py
@@ -2,7 +2,8 @@ import os
 import logging
 import vertexai
 from vertexai.generative_models import GenerativeModel
-from app.utils import exponential_backoff, get_llm_response
+from app.utils.bq_utils import fetch_context, insert_data_into_bigquery
+from app.utils.llm_utils import get_llm_response, exponential_backoff
 
 logging.basicConfig(level=logging.INFO)
 
@@ -10,6 +11,8 @@ logging.basicConfig(level=logging.INFO)
 PROJECT_ID = os.getenv("PROJECT_ID", "coursecompass")
 LOCATION = os.getenv("LOCATION", "us-east1")
 ENDPOINT_ID = os.getenv("ENDPOINT_ID", "projects/542057411868/locations/us-east1/endpoints/8473016923548811264")
+DATASET_ID = os.getenv("DATASET_ID", "mlopsdataset")
+USER_TABLE_NAME = os.getenv("USER_TABLE_ID", "user_data_table")
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
@@ -19,13 +22,23 @@ def process_llm_request(query: str) -> str:
     
     model = GenerativeModel(model_name=ENDPOINT_ID)
     
-    context = fetch_context(query)
+    # Fetch context
+    context = fetch_context(query, PROJECT_ID)
+    
     full_prompt = f"Context: {context}\n\nQuery: {query}"
     
-    return get_llm_response(full_prompt, model)
-
-def fetch_context(query: str) -> str:
-    # TODO: implement logic to fetch context from the BQ table
-    context = ""
+    # Generate response
+    response = get_llm_response(full_prompt, model)
     
-    return context
+    user_data_row = [
+        {
+            "question": query,
+            "context": context,
+            "response": response
+        }
+    ]
+    
+    # Insert data into BigQuery
+    insert_data_into_bigquery(PROJECT_ID, DATASET_ID, USER_TABLE_NAME, user_data_row) 
+    
+    return response
